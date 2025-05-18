6ea6ace7d7f181ba8201852f7b2011bad27ec077
blduffie <blduffie@gmail.com>
2025-05-18 13:04:27 -0400

    chore: integrate design system

--- test-project/web-client/tailwind.config.js
+++ test-project/web-client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('linq-ds/tailwind/themes/linq')],
   content: ["./src/**/*.{html,scss,ts}"],
   theme: {
     extend: {},
